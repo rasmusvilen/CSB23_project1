2ce72de6c36be4b693068f5ecc66cb27f46b7eb4
rasmusvilen <113108280+rasmusvilen@users.noreply.github.com>
2023-12-28 18:40:26 +0200

    Delete mysite directory

--- mysite/polls/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import UserAccount
-
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = UserAccount
-        fields = ['username', 'password']
