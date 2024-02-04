5e842f673d8efc2fe9273dce239d3537e03564ad
rasmusvilen <113108280+rasmusvilen@users.noreply.github.com>
2024-02-04 15:54:52 +0200

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,17 +13,22 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        # Flaw 1, Injection, rows 17-23
+        # Flaw 1, Injection, rows 17-23, specifically 18-20
         user_input = self.request.GET.get('user_input', '')
-        query = "SELECT * FROM polls_question WHERE question_text LIKE %s ORDER BY pub_date DESC LIMIT 5"
+        query = f"SELECT * FROM polls_question WHERE question_text LIKE '%{user_input}%' ORDER BY pub_date DESC LIMIT 5"
         with connection.cursor() as cursor:
-            cursor.execute(query, ['%' + user_input + '%'])
+            cursor.execute(query)
             result_list = cursor.fetchall()
         questions = [Question(id=row[0], question_text=row[1], pub_date=row[2]) for row in result_list]
         return questions
-        # Fix to flaw 1, rows 25-26
-        """user_input = self.request.GET.get('user_input', '')
-        return Question.objects.filter(question_text__icontains=user_input).order_by('-pub_date')[:5]"""
+        # Fix to flaw 1, rows 27 and 28
+        '''user_input = self.request.GET.get('user_input', '')
+        return Question.objects.filter(question_text__icontains=user_input).order_by('-pub_date')[:5]'''
+
+        # A short explanation of the change I made.
+
+        '''Apparently my initial injection flaw was incorrect as the parameter was in fact escaped properly. However, I do now believe that my new code contains an actual
+        injection flaw. Now user input is added unsanitized directly into the SQL query, which could result in an injection vulnerability. The fix remains the same.'''
 
 class DetailView(generic.DetailView):
     model = Question
@@ -52,10 +57,10 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            # Part of Flaw 2, row 56 (saving user with unhashed password)
+            # Part of Flaw 2, row 61 (saving user with unhashed password)
             form.save()
             return redirect(reverse('polls:index'))
-            # Fix to flaw 2, rows 59-62
+            # Fix to flaw 2, rows 64-67
             """user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])  # Hashing the password
             user.save()
